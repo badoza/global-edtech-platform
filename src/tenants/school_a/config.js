4a46683a9dab5cb217fd90ca6b5d6255dc7a9fb9
badoza <dodge.kunal@gmail.com>
2026-04-24 19:40:25 +0530

    Create config.js

---
+++ src/tenants/school_a/config.js
@@ -0,0 +1,9 @@
+export const config = {
+  tenantId: 'school_a_001',
+  brandName: 'Global Excellence Academy',
+  colors: {
+    primary: '#1A1A40',
+    secondary: '#00A8E8',
+    background: '#F8FAFC'
+  }
+};

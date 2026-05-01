d8bbc2f4753414f2dbcfaf20225cd6f72729629f
badoza <dodge.kunal@gmail.com>
2026-05-01 19:08:00 +0530

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/global-edtech-platform',
-  assetPrefix: '/global-edtech-platform/',
-  images: { unoptimized: true }
-};
-
-export default nextConfig;

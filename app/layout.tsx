15f2c054fa88c542ff805536335fc3a6995406b0
badoza <dodge.kunal@gmail.com>
2026-05-01 18:38:36 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+import './globals.css'
+import React from 'react'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="bg-slate-50 text-slate-900">{children}</body>
+    </html>
+  )
+}

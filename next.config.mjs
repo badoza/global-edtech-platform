f7d6c6317f7e33f52db148153baea30df9c5bdd7
badoza <dodge.kunal@gmail.com>
2026-05-01 18:37:15 +0530

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/global-edtech-platform',
+  assetPrefix: '/global-edtech-platform/',
+  images: { unoptimized: true }
+};
+
+export default nextConfig;

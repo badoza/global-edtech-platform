30e465f12265b51be3249d2e9763f37c1a88a0e5
badoza <dodge.kunal@gmail.com>
2026-05-01 18:36:00 +0530

    Delete src directory

--- src/tenants/school_a/config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-  tenantId: '44d48041-ed3b-4b4f-8315-87c993ecb440',
-  brandName: 'Global Excellence Academy',
-  colors: {
-    primary: '#1A1A40',
-    secondary: '#00A8E8',
-    background: '#F8FAFC'
-  }
-};
